5b551d828ac0e88e0c539fc43c417a4257fc7100
Niqori <kuuhakuwatashi@gmail.com>
2025-10-08 16:18:59 +0800

    test

--- tugas/tugas-fundamental-js/looping.js
+++ tugas/tugas-fundamental-js/looping.js
@@ -29,8 +29,7 @@ for ( var angka = 1; angka <= 20 ; angka++){
 
 //TUGAS NOMOR 3
 
-for ( var angka = 1; angka <= 20 ; angka++)
-    {
+for ( var angka = 1; angka <= 20 ; angka++){
     if (angka % 3 === 0 && angka % 2 !== 0){
         console.log(angka," - i love coding")
     }
@@ -43,4 +42,27 @@ for ( var angka = 1; angka <= 20 ; angka++)
     else{
         console.log(angka," - Santai")
     }
+}
+
+//TUGAS NOMOR 4
+for (var i = 1; i <= 7; i++) {
+    var hasil = "";
+    for (var j = 1; j <= i; j++) {
+        hasil += "#";
+    }
+    console.log(hasil);
+}
+
+
+//TUGAS NOMOR 5
+
+var sentence = "Fullstack Developer"
+
+for (var i=0;i<sentence.length;i++)
+{
+    var char = sentence[i].toLowerCase();
+    if (char !== "a" && char !== "e" && char !== "i" && char !== "o" && char !== "u" && char !== " "){
+    console.log(sentence[i]); // tampilkan huruf aslinya (bisa juga char)
+    }
+
 }
