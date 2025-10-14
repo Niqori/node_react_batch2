06c410d7ed502271ac9427193b78827ebe35dfb4
Niqori <kuuhakuwatashi@gmail.com>
2025-10-14 09:32:53 +0800

    Async OOP

--- tugas/tugas-asynchronouse-javascript/index3.js
+++ tugas/tugas-asynchronouse-javascript/index3.js
@@ -1,12 +1,6 @@
 var filterBooksPromise = require('./promise2.js')
  
 
-// Lanjutkan code untuk menjalankan function filterBookPromise
-// gunakan promise dengan kondisi seperti di bawah ini:
-// bukunya berwarna dan jumlah halamannya 50
-// bukunya tidak berwarna dan jumlah halamannya 250 (gunakan async/await untuk kondisi ini)
-// bukunya berwarna dan jumlah halamannya 30 (gunakan async/await untuk kondisi ini) 
-
 var filterBooksPromise = require('./promise2.js');
 
 // 🔹 1. Pakai .then() dan .catch()
---
+++ tugas/tugas-asynchronouse-javascript/index4.js
@@ -0,0 +1,41 @@
+
+var filterCarsPromise = require("./promise3.js");
+
+filterCarsPromise("black", 2019)
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err.message));
+
+filterCarsPromise("silver", 2017)
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err.message));
+
+const invalidCar = async () => {
+  try {
+    const result = await filterCarsPromise("grey", 2019);
+    console.log(result);
+  } catch(err) {
+    console.log(err.message)
+  }
+};
+invalidCar()
+
+const greyCar = async () => {
+  try {
+    const result = await filterCarsPromise("grey", 2018);
+    console.log(result);
+  } catch(err) {
+    console.log(err.message)
+  }
+};
+greyCar()
+
+const blackCar = async () => {
+  try {
+    const result = await filterCarsPromise("black", 2020);
+    console.log(result);
+  } catch(err) {
+    console.log(err.message)
+  }
+};
+blackCar()
+
